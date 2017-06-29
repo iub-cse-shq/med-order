9f2b1974820748cb1ff500421059ee947203aab5
tahmidf <1420283@iub.edu.bd>
2017-06-29 16:35:10 +0000

    All Forms Working No database

--- controllers/core.server.controller.js
+++ controllers/core.server.controller.js
@@ -10,15 +10,70 @@ exports.index = function(req, res) {
 	});
 };
 
+exports.about = function(req, res) {
+	res.render('./../about.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
 exports.add_product = function(req, res) {
 	res.render('./../public/add_product.ejs', {
 		user: req.user || null,
 		request: req
 	});
 };
+exports.add_purchase = function(req, res) {
+	res.render('./../public/add_purchase.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
 
-exports.about = function(req, res) {
-	res.render('./../about.ejs', {
+exports.manage_product = function(req, res) {
+	res.render('./../public/manage_product.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+exports.payment = function(req, res) {
+	res.render('./../public/payment.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+exports.stock = function(req, res) {
+	res.render('./../public/stock.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+exports.supplier = function(req, res) {
+	res.render('./../public/supplier.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+exports.user_buy_med = function(req, res) {
+	res.render('./../public/user_buy_med.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+
+exports.user_index = function(req, res) {
+	res.render('./../public/user_index.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+exports.user_pre_order = function(req, res) {
+	res.render('./../public/user_pre_order.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+exports.view_invoices = function(req, res) {
+	res.render('./../public/view_invoices.ejs', {
 		user: req.user || null,
 		request: req
 	});
--- routes/core.server.routes.js
+++ routes/core.server.routes.js
@@ -6,4 +6,13 @@ module.exports = function(app) {
 	app.route('/').get(core.index);
 	app.route('/about').get(core.about);
 	app.route('/add_product').get(core.add_product);
+	app.route('/add_purchase').get(core.add_purchase);
+	app.route('/manage_product').get(core.manage_product);
+	app.route('/payment').get(core.payment);
+	app.route('/stock').get(core.stock);
+	app.route('/supplier').get(core.supplier);
+	app.route('/view_invoices').get(core.view_invoices);
+	app.route('/user_buy_med').get(core.user_buy_med);
+	app.route('/user_index').get(core.user_index);
+	app.route('/user_pre_order').get(core.user_pre_order);
 };
